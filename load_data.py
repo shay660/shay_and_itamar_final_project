be80c2ac1458baeb844ab9cfc9e7aa347647724f
shay660-1 <shay.itzhaki@mail.huji.ac.il>
2023-08-24 15:39:13 +0300

    data redy - not efficient

--- load_data.py
+++ load_data.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 
 
-def load_seq_data(path: str, min_length: int, max_length: int) -> pd.DataFrame:
+def load_seq_data(samples_path: str, response_path: str, min_length: int,
+                  max_length: int) -> pd.DataFrame:
     """
     loads the data from a text file into data frame that count how many times
     each sequence in the length of 3-7 nucleotides is in each 3'URR.
@@ -13,7 +14,7 @@ def load_seq_data(path: str, min_length: int, max_length: int) -> pd.DataFrame:
     :return: data frame that count how many times each sequence in the length of
      3-7 nucleotides is in each 3'URR. The shape (n_genes, 4^3+4^4+4^5+4^6+4^7)
     """
-    f: list = open(path, "r").readlines()
+    f: list = open(samples_path, "r").readlines()
 
     # make all the possible sequence in the length of 3-7 nt.
     characters = 'ATGC'
@@ -32,12 +33,14 @@ def load_seq_data(path: str, min_length: int, max_length: int) -> pd.DataFrame:
         k_mers_counter[id] = k_mers_count(seq, min_length, max_length,
                                           all_sequences)
 
-    df: pd.DataFrame = pd.DataFrame.from_dict(k_mers_counter, orient="index")
+    samples = pd.DataFrame.from_dict(k_mers_counter, orient="index")
+    response_vec = load_response(response_path)
+    df: pd.DataFrame = samples.join(response_vec, how="inner")
     return df
 
 
 def k_mers_count(seq: str, min_length: int, max_length: int,
-                 all_sequences: List[str]):
+                 all_sequences: List[str]) -> Dict[str, int]:
     """
 
     :param seq:
@@ -59,3 +62,21 @@ def k_mers_count(seq: str, min_length: int, max_length: int,
             # Increment the count for this kmer
             counts[kmer] += 1
     return counts
+
+
+def load_response(path: str) -> pd.DataFrame:
+    """
+    load the respon
+    :param path:
+    :return:
+    """
+    lines: List[str] = open(path, "r").readlines()
+    id_to_rate: Dict[str: float] = {}
+    for line in lines[1:]:
+        line = line.split()
+        id, deg_rate = line[0], line[1]
+        id_to_rate[id] = deg_rate
+
+    df: pd.DataFrame = pd.DataFrame.from_dict(id_to_rate, orient="index")
+    df.columns =  ['degradation rate']
+    return df
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from time import time
 
-from load_data import load_seq_data
+import pandas as pd
 
+from load_data import load_seq_data
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 
 if __name__ == '__main__':
     start = time()
-    load_seq_data("../data/small_data_samples.txt", 3, 7)
+    df: pd.DataFrame = load_seq_data("./data/small_data_samples.txt",
+                                     "./data/3U.models.3U.00A.seq1022_param.txt",
+                                     3, 7)
     end = time()
-    print(end - start)
