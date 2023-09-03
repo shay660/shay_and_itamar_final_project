892cef607586f6653d17c0791a83eb15b5f6992e
itanini <itamar.nini@mail.huji.ac.il>
2023-09-03 09:18:10 +0300

    reorganized and documented code

--- load_data.py
+++ load_data.py
@@ -4,31 +4,6 @@ import pandas as pd
 from pandas import DataFrame
 from tqdm import tqdm
 
-
-def load_seq_data(samples_path: str, responses_vec: pd.DataFrame, min_length:
- int, max_length: int) -> pd.DataFrame:  # pd.DataFrame, pd.DataFrame
-    """
-    loads the data from a text file into data frame that count how many times
-    each sequence in the length of 3-7 nucleotides is in each 3'URR.
-    :param samples_path: path to a text file
-    :param responses_vec:
-    :param late_onset_responses:
-    :param min_length:
-    :param max_length:
-    :return: Two data frame that count how many times each sequence in the
-    length of 3-7 nucleotides is in each 3'URR. one for the early onset and
-    the other for late onset sequences. The shape (n_genes, 4^3+4^4+4^5+4^6+4^7)
-    """
-
-    # loads the responses vector
-    # responses_vec, late_onset_responses = load_response(
-    #     response_path)
-
-    df = _matrix_generator(f, max_length, min_length,
-                           responses_vec)
-    return df
-
-
 def _matrix_generator(f: List[str], response_vec,min_length: int,
                       max_length:int) -> pd.DataFrame:
     """
@@ -41,19 +16,18 @@ def _matrix_generator(f: List[str], response_vec,min_length: int,
     :return: data frame that count how many times each sequence in the length of
      3-7 nucleotides is in each 3'URR. The shape (n_genes, 4^3+4^4+4^5+4^6+4^7)
     """
-    # create dict. key is id, value is a dict where the key is k_mer and the
-    # value is its frequency in the id seq.
+    # create dict. key is id, value is a dict where the key is k_mer and the value is its frequency in the id seq.
     k_mers_counter: Dict[str: Dict[str, int]] = {}
     for line in tqdm(f[2:]):
         line = line.split()
         id, seq = line[0], line[1]
-        #  checks if we have the response value for the id before loading it
-        #  to the dataset
+
+        #  checks if we have the response value for the id before loading it to the dataset
         if id in response_vec.index.tolist():
             k_mers_counter[id] = k_mers_count(seq, min_length, max_length)
+
     samples = pd.DataFrame.from_dict(k_mers_counter, orient="index", dtype=int)
     samples.fillna(0, inplace=True)
-    # samples: pd.DataFrame = samples.join(response_vec, how="inner")
     return samples
 
 
@@ -89,19 +63,11 @@ def load_response(path: str, ) -> DataFrame:
     :return: pandas DataFrame of the responses.
     """
     lines: List[str] = open(path, "r").readlines()
-    # id_to_rate: Dict[str: float] = {}
-    # id_to_x0: Dict[str: float] = {}
-    all_onset_dict = {}
+    responses_dict = {}
     for line in lines[1:]:
         line = line.split()
         id, deg_rate, x0, t0 = line[0], line[1], line[2], line[3]
-        # id_to_rate[id],id_to_x0[id] = deg_rate, x0
-        all_onset_dict[id] = [deg_rate, x0, t0]
-
-    # rate_df: pd.DataFrame = pd.DataFrame.from_dict(id_to_rate, orient="index")
-    # x0_df: pd.DataFrame = pd.DataFrame.from_dict(id_to_x0, orient="index")
-    # samples: pd.DataFrame = rate_df.join(x0_df, how= "inner")
-    all_onset_df: pd.DataFrame = pd.DataFrame.from_dict(all_onset_dict,
-                                                        orient="index")
-    all_onset_df.columns = ['degradation rate', 'x0', 't0']
-    return all_onset_df
+        responses_dict[id] = [deg_rate, x0, t0]
+    responses_df: pd.DataFrame = pd.DataFrame.from_dict(responses_dict,orient="index")
+    responses_df.columns = ['degradation rate', 'x0', 't0']
+    return responses_df
--- main.py
+++ main.py
@@ -4,37 +4,51 @@ from typing import Dict, List
 
 import pandas as pd
 
-from load_data import load_seq_data, load_response, _matrix_generator
+from load_data import load_response, _matrix_generator
 from sklearn.linear_model import Lasso, LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import GridSearchCV
 
 
-def model_generator(samples: pd.DataFrame, response_vec: pd.DataFrame,
-                    alphas: list):
+def model_generator(samples: pd.DataFrame, response_vec: pd.DataFrame, alphas: list) -> None:
+    """
+    Generate and evaluate a predictive model using Lasso regression followed by Linear Regression.
+    :param samples: Input data
+    :param response_vec: responses data
+    :param alphas: List of alpha values for Lasso regularization
+    :return: None
+    """
+
+    # merge samples with responses iff we have the needed data for both. many samples are filtered because
+    # we don't have a proper response for them
     samples = samples.join(response_vec, how="inner")
-    X_train, X_test, y_train, y_test = train_test_split(samples.iloc[:, :-3],
-                                                        samples.iloc[:, -3:],
-                                                        test_size=0.1,
+    X_train, X_test, y_train, y_test = train_test_split(samples.iloc[:, :-3],samples.iloc[:, -3:], test_size=0.1,
                                                         random_state=1)
+
     lasso_model = Lasso(max_iter=5000)
+    # set all possible values for the regularization term
     alphas: Dict[str, List[float]] = {'alpha': alphas}
-    grid_search = GridSearchCV(lasso_model, alphas, cv=10,
-                               scoring='neg_mean_squared_error').fit(X_train,
-                                                                     y_train)
+
+    # cross validation model
+    grid_search = GridSearchCV(lasso_model, alphas, cv=10, scoring='neg_mean_squared_error').fit(X_train, y_train)
+
     best_alpha = grid_search.best_params_['alpha']
-    best_score = -grid_search.best_score_  # Convert back to positive
     print(f"Best Alpha: {best_alpha}")
+
+    # chose only the sequences that effects the desegregation rate according to the lasso model.
     lasso_model = grid_search.best_estimator_
-    # chose only the sequences that effects the desegregation rate according
-    # to the lasso model.
     X_train = X_train.loc[:, (lasso_model.coef_ != 0).any(axis=0)]
     X_test = X_test.loc[:, X_train.columns]
+
+    # creates a linear regression model with the features as the non-zero coefficients of the lasso
     linear_reg_model = LinearRegression().fit(X_train, y_train)
+
+    # estimation
     prediction = linear_reg_model.predict(X_test)
     mse = mean_squared_error(y_test, prediction)
     r2 = r2_score(y_test, prediction)
+
     print(f"MSE of the Linear regression = {mse}")
     print(f"r2 of the Linear regression = {r2}")
 
@@ -42,47 +56,38 @@ def model_generator(samples: pd.DataFrame, response_vec: pd.DataFrame,
 if __name__ == '__main__':
     start = time()
 
+    # reads arguments
+    f: list = open(sys.argv[1], "r").readlines()
+    responses_with_polyA = load_response(sys.argv[2])
     responses_without_polyA = load_response(
         sys.argv[3])
-    # load responses vectors
+
+    # filter samples with t0=1 to be the early on-set vectors
     early_responses_without_polyA = responses_without_polyA[
         responses_without_polyA['t0'] == '1']
-    # polyA tail.
-
-    responses_with_polyA = load_response(
-        sys.argv[2])  # load responses with
-    # polyA tail.
     early_responses_with_polyA = responses_with_polyA[
         responses_with_polyA['t0'] == '1']
 
-    f: list = open(sys.argv[1], "r").readlines()
-    # load samples
+    # creates 4 dataframes
     samples_with_polyA = _matrix_generator(f, responses_with_polyA, 3, 7)
-    samples_without_polyA = _matrix_generator(f,
-                                          responses_without_polyA, 3, 7)
-    early_samples_with_polyA = _matrix_generator(f,
-                                                 early_responses_with_polyA, 3, 7)
-    early_samples_without_polyA = _matrix_generator(f,
-                                        early_responses_without_polyA, 3, 7)
+    samples_without_polyA = _matrix_generator(f,responses_without_polyA, 3, 7)
+    early_samples_with_polyA = _matrix_generator(f,early_responses_with_polyA, 3, 7)
+    early_samples_without_polyA = _matrix_generator(f,early_responses_without_polyA, 3, 7)
 
     print(f"load data time = {time() - start}")
 
     #  generate models with all the possible combinations.
     print("Early-onset with PolyA")
-    model_generator(early_samples_with_polyA, early_responses_with_polyA,
-                    [0.005, 0.008, 0.01])
+    model_generator(early_samples_with_polyA, early_responses_with_polyA,[0.005, 0.008, 0.01])
     print("**********************************")
     print("Early-onset without PolyA")
-    model_generator(early_samples_without_polyA, early_responses_without_polyA,
-                    [0.005, 0.008, 0.01])
+    model_generator(early_samples_without_polyA, early_responses_without_polyA,[0.005, 0.008, 0.01])
     print("**********************************")
     print("All data with PolyA")
-    model_generator(samples_with_polyA, responses_with_polyA, [0.005, 0.008,
-                                                               0.01])
+    model_generator(samples_with_polyA, responses_with_polyA, [0.005, 0.008,0.01])
     print("**********************************")
     print("All data without PolyA")
-    model_generator(samples_without_polyA, responses_without_polyA, [0.005,
-                                                                     0.008, 0.01])
+    model_generator(samples_without_polyA, responses_without_polyA, [0.005,0.008, 0.01])
     print("**********************************")
     end = time()
     print(f"time = {end - start}")
