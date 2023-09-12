7ac331a4dabc2dd878866374aceedfcc7ec6fceb
shay660-1 <shay.itzhaki@mail.huji.ac.il>
2023-09-12 10:31:11 +0300

    with linear regression function and model_generator.py file - with documentation.

--- model_generator.py
+++ model_generator.py
@@ -9,9 +9,7 @@ from sklearn.model_selection import GridSearchCV
 def linear_reg_model_generator(samples: pd.DataFrame) -> Tuple[object,
                                                        pd.DataFrame, pd.Series]:
     """
-
-    :param samples:
-    :return:
+    fit a linear regression model with samples
     """
     # X_train, y_train, X_test, y_test = _split_to_train_and_test(samples)
     X_train, y_train = samples.iloc[:, :-3], samples.iloc[:, -3:]
@@ -22,6 +20,8 @@ def linear_reg_model_generator(samples: pd.DataFrame) -> Tuple[object,
 
 
 def _split_to_train_and_test(samples: pd.DataFrame):
+    """ split the data to train and test set and for samples and responses
+    vector. It's also prints the progress of the process."""
     X_train, X_test, y_train, y_test = train_test_split(samples.iloc[:, :-3],
                                                         samples.iloc[:, -3:],
                                                         test_size=0.0,
@@ -33,7 +33,7 @@ def _split_to_train_and_test(samples: pd.DataFrame):
 
 
 def lasso_and_cross_validation_generator(samples: pd.DataFrame, alphas: list) \
-        -> object:
+        -> Tuple[object, pd.DataFrame, pd.Series]:
     """
     Generate and evaluate a predictive model using Lasso regression followed by
     Linear Regression.
@@ -41,9 +41,9 @@ def lasso_and_cross_validation_generator(samples: pd.DataFrame, alphas: list) \
     :param alphas: List of alpha values for Lasso regularization
     :return: None
     """
-    X_train, y_train, X_test, y_test,  = _split_to_train_and_test(samples)
-
-    lasso_model = Lasso(max_iter=2500, tol=5e-3)
+    # X_train, y_train, X_test, y_test = _split_to_train_and_test(samples)
+    X_train, y_train = samples.iloc[:, :-3], samples.iloc[:, -3:]
+    lasso_model = Lasso(max_iter=2500, tol=3e-3)
     # set all possible values for the regularization term
     alphas: Dict[str, List[float]] = {'alpha': alphas}
 
@@ -61,7 +61,7 @@ def lasso_and_cross_validation_generator(samples: pd.DataFrame, alphas: list) \
     # the lasso model.
     lasso_model = fitted_grid_search.best_estimator_
     X_train = X_train.loc[:, (lasso_model.coef_ != 0).any(axis=0)]
-    X_test = X_test.loc[:, X_train.columns]
+    # X_test = X_test.loc[:, X_train.columns]
 
     print(f"Number of non-zeroes weightS features after the Lasso: "
           f"{X_train.shape[1]}", flush=True)
@@ -71,6 +71,6 @@ def lasso_and_cross_validation_generator(samples: pd.DataFrame, alphas: list) \
     linear_reg_model = LinearRegression().fit(X_train, y_train)
     print(f"**** Linear Regression Fitted ****", flush=True)
 
-    return linear_reg_model, X_test, y_test
+    return linear_reg_model, X_train, y_train  # X_test, y_test
 
 
