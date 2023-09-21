8b8d00c562ed80ef35fc70aeb89bfaa1e11f79ca
itanini <itamar.nini@mail.huji.ac.il>
2023-09-21 13:26:19 +0300

    fixed README for run_model

--- run_model.py
+++ run_model.py
@@ -10,6 +10,7 @@ from main import predict_and_calculate_loss
 if __name__ == '__main__':
     model = joblib.load(sys.argv[1])
     data = pd.read_csv(sys.argv[2])
+    name = sys.argv[3]
 
-    predict_and_calculate_loss(model, data[model.feature_names_in_], data.iloc[:,- 3:])
+    predict_and_calculate_loss(model, data[model.feature_names_in_], data.iloc[:, - 3:], _name_of_model=name)
 
