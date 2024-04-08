91ed384fdb5ef6474b3c82af11c2a20a1972dd40
shay660-1 <shay.itzhaki@mail.huji.ac.il>
2024-04-08 12:11:27 +0300

    find the most significant kmers from existing linear regression model.

---
+++ filter_motifs/linear_regression_kmers.py
@@ -0,0 +1,18 @@
+import glob
+import sys
+from os import chdir
+
+import joblib
+from main import find_significant_kmers
+
+
+def significant_kmers_from_model() -> None:
+    model_path = glob.glob("*.joblib")[0]
+    print(model_path)
+    model = joblib.load(model_path)
+    find_significant_kmers(model)
+
+if __name__ == '__main__':
+    dir_path = sys.argv[1]
+    chdir(dir_path)
+    significant_kmers_from_model()
