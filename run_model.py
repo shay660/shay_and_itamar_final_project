a49a337d5c4f2524f7aeca8623a7f8fd506fa546
shay660-1 <shay.itzhaki@mail.huji.ac.il>
2024-05-23 11:01:57 +0300

    fix the run_model file.

--- run_model.py
+++ run_model.py
@@ -2,13 +2,25 @@ import sys
 from os import mkdir, chdir
 import datetime
 import joblib
+import numpy as np
 import pandas as pd
+
+import load_data
 from main import predict_and_calculate_loss, find_significant_kmers
+
+def count_kmers_according_to_lasso(df: pd.DataFrame, kmers: np.array) ->pd.DataFrame:
+    # Initialize a new DataFrame with the same index as the original DataFrame
+    kmer_counts_df = pd.DataFrame(index=df.index)
+
+    # Iterate over each k-mer and count its occurrences in each sequence
+    for kmer in kmers:
+        kmer_counts_df[kmer] = df['seq'].apply(lambda seq: seq.count(kmer))
+    return kmer_counts_df
 
 
 def main():
     model = joblib.load(sys.argv[1])
-    data = pd.read_csv(sys.argv[2])
+    data = pd.read_csv(sys.argv[2], index_col='id')
     name = sys.argv[3]
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     directory_name = f"./models/{name}_{timestamp}"
@@ -17,16 +29,18 @@ def main():
     file = open("summary.txt", "w")
     file.write(f"Model {name}\n")
     file.write(f"Run at {timestamp}\n")
-    model_features = model.feature_names_in_
-    find_significant_kmers(model)
-    new_columns = pd.DataFrame({feat: [0] for feat in model_features},
-                               index=data.index)
+    model_features: np.array = model.feature_names_in_
+    samples = count_kmers_according_to_lasso(data, model_features)
+    # new_columns = pd.DataFrame({feat: [0] for feat in model_features},
+    #                            index=data.index)
     # Select only the columns that do not already exist in the data DataFrame
-    new_columns = new_columns.loc[:, ~new_columns.columns.isin(data.columns)]
+    # new_columns = new_columns.loc[:, ~new_columns.columns.isin(data.columns)]
     # Concatenate the new_columns DataFrame with the original data DataFrame
-    data = pd.concat([new_columns, data], axis=1)
-    predict_and_calculate_loss(model, data[model.feature_names_in_],
-                               data.iloc[:, -3:], name, file)
+    # data = pd.concat([new_columns, data], axis=1)
+    # predict_and_calculate_loss(model, data[model.feature_names_in_],
+    #                            data.iloc[:, -3:], name, file)
+    prediction = model.predict(samples)
+    print(prediction)
     file.close()
 
 
