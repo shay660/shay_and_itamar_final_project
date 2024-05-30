2fa725994bd4e8d727861c71c81bc79e778b6b28
shay660-1 <shay.itzhaki@mail.huji.ac.il>
2024-05-30 16:29:47 +0300

    plot prediction vs true values, the linear regression is calculated here

--- plot_prediciton_vs_true.py
+++ plot_prediciton_vs_true.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 expr_rate = pd.read_csv(
     "./Neural_network/DeepUTR-main/files/5utr_dataset/data.expr.5UTR_40A.csv"
@@ -11,28 +13,33 @@ expr_rate.drop('6hr.1', axis=1, inplace=True)
 lr_as_true_res = pd.read_csv(
     "./Neural_network/DeepUTR-main/files/5utr_dataset/data.dg.5UTR_40A.csv",
     index_col='id', skiprows=0)
-lasso_pred = pd.read_csv("./results/prediction_of_seqs_with_motifs.csv",
-
-                         index_col=0, header=None)
+lasso_pred = pd.read_csv("./data/final_seqs_with_pred.csv",
+                         index_col='id')
+lasso_pred.drop_duplicates(inplace=True)
 
 for seq_id, dg in lasso_pred.iterrows():
     xes = [0, 1, 3, 5, 6, 7, 8]
 
-    plt.plot(xes[1:], [expr_rate.loc[seq_id][f"{x}hr"] for x in xes[1:]],
-             marker='o', linestyle='none')
-
     x0 = lr_as_true_res.loc[seq_id]['x0']
+    y = [x0] + [expr_rate.loc[seq_id][f"{x}hr"] for x in xes[1:]]
 
-    lasso_pred_func = lambda x: (-dg[1] * x) + x0
+    plt.plot(xes
+             , y,
+             marker='o', linestyle='none')
+    lasso_pred_func = lambda x: (dg.loc['pred_dg'] * x) + x0
     lasso_predictions = [lasso_pred_func(x) for x in xes]
     plt.plot(xes, lasso_predictions, label="Lasso prediction")
 
-    lr_dg = lr_as_true_res.loc[seq_id]['dg']
-    lr_func = lambda x: -lr_dg * x + x0
-    lr_predictions = [lr_func(x) for x in xes]
-    plt.plot(xes, lr_predictions, label="Linear Regression")
+    # lr_dg = lr_as_true_res.loc[seq_id]['dg']
+    # lr_func = lambda x: -lr_dg * x + x0
+    # lr_predictions = [lr_func(x) for x in xes]
+    coef = np.polyfit(xes, y, 1)
+    polynom = np.poly1d(coef)
+    trandline_y = polynom(xes)
+    plt.plot(xes, trandline_y, label="Linear Regression")
 
     plt.title(seq_id)
     plt.legend()
     plt.savefig(f"./results/predVsTrue/{seq_id}.png")
+    plt.figure(figsize=(12, 8))
     plt.clf()
