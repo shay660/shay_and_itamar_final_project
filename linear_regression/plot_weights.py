74394a02c5dd02e0442d211608d6cb6e673acacc
itanini <itamar.nini@mail.huji.ac.il>
2024-05-30 16:30:04 +0300

    made all graphs

--- linear_regression/plot_weights.py
+++ linear_regression/plot_weights.py
@@ -1,21 +1,47 @@
 import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Load the data
 WEIGHTS_PATH = "data_for_linear_reg/weights_data/weights_vector.csv"
-SEQUENCE_PATH = "data_for_linear_reg/mRNA_sequences_for_5utr_40A.csv"
-# weights= pd.read_csv(WEIGHTS_PATH, index_col= 'Feature')
+SEQUENCE_PATH = "data_for_linear_reg/final_all.csv"
 
-sequences = pd.read_csv(SEQUENCE_PATH)
+sequences = pd.read_csv(SEQUENCE_PATH, index_col=0, header=0).drop_duplicates()
 
-model = joblib.load('../models/all_data_early_onset_without_polyA_2023-09-27_11-43-19/all_data_early_onset_without_polyA_model.joblib')
-weighter = model.coef_[0, :]
-k_mer_names= model.feature_names_in_
-weights = pd.DataFrame({'k_mer': k_mer_names, 'weight': weighter}).set_index('k_mer')
+no_lasso_model = joblib.load('../linear_regression/data_for_linear_reg/models/5utr_no_lasso_model.joblib')
+lasso_model = joblib.load("../linear_regression/data_for_linear_reg/models/filtered_data_5utr_40A_model.joblib")
 
-pass
+no_lasso_weights = no_lasso_model.coef_[0, :]
+lasso_weights = lasso_model.coef_[0, :]
 
-for index, sequence in sequences['seq'].items():  # Iterate over sequences along with their index
+no_lasso_k_mer_names = no_lasso_model.feature_names_in_
+lasso_k_mer_names = lasso_model.feature_names_in_
+
+weights = pd.DataFrame({'k_mer': no_lasso_k_mer_names, 'weight': no_lasso_weights}).set_index('k_mer')
+lasso_weights_df = pd.DataFrame({'k_mer': lasso_k_mer_names, 'weight': lasso_weights}).set_index('k_mer')
+
+
+# Define a function to plot sequence weights
+def plot_sequence_weights(seq_weights, seq_arr, title, filepath):
+    plt.figure(figsize=(20, 6))
+    sns.set(style="whitegrid")
+    sns.lineplot(x=seq_weights['pos'], y=seq_weights['weight'], marker='o', color='b', linewidth=2.5, markersize=8)
+
+    plt.xticks(range(1, len(seq_arr) + 1), seq_arr, fontsize=10)
+    plt.yticks(fontsize=12)
+    plt.title(title, fontsize=18, fontweight='bold')
+    plt.xlabel('Position', fontsize=14)
+    plt.ylabel('Weight', fontsize=14)
+    plt.ylim(-0.06, 0.06)
+    plt.tight_layout()
+    plt.savefig(filepath, dpi=300)
+    plt.close()
+
+
+# Iterate over sequences for no_lasso_weights
+for sample in sequences.index:
+    sequence = sequences.loc[sample]['seq']
     seq_weights = {'pos': [], 'weight': []}
     for pos in range(len(sequence)):
         seq_weights['pos'].append(pos + 1)
@@ -29,17 +55,26 @@ for index, sequence in sequences['seq'].items():  # Iterate over sequences along
                 continue
         seq_weights['weight'].append(pos_weight)
 
-    # seq_arr = [f"{char}\n{pos}" for char, pos in zip(sequence, range(1, len(sequence) + 1))]
     seq_arr = list(sequence)
-    # Plotting
-    plt.plot(seq_weights['pos'], seq_weights['weight'], marker='o')
+    plot_sequence_weights(seq_weights, seq_arr, f"{sample} - No Lasso",
+                          f'results/5utr_k_mer_plots_no_lasso/sequence_{sample}_no_lasso.png')
 
-    # Set x-axis ticks and labels
-    plt.xticks(range(1, len(sequence) + 1), seq_arr)
+# Iterate over sequences for lasso_weights
+for sample in sequences.index:
+    sequence = sequences.loc[sample]['seq']
+    seq_weights = {'pos': [], 'weight': []}
+    for pos in range(len(sequence)):
+        seq_weights['pos'].append(pos + 1)
+        pos_weight = 0
+        for k_len in range(3, 8):
+            try:
+                k_mer = sequence[pos: pos + k_len]
+                if k_mer in lasso_weights_df.index:
+                    pos_weight += float(lasso_weights_df.loc[k_mer])
+            except:
+                continue
+        seq_weights['weight'].append(pos_weight)
 
-    # Save and show plot
-    plt.savefig(f'results/3utr_k_mer_plots/sequence_{index + 1}.png')
-    plt.show()
-    plt.clf()
-
-
+    seq_arr = list(sequence)
+    plot_sequence_weights(seq_weights, seq_arr, f"{sample} - Lasso",
+                          f'results/5utr_k_mer_plots_lasso/sequence_{sample}_lasso.png')
