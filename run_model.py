3fa22ef44f8e109e33f239ccb63cdf815341575d
shay660-1 <shay.itzhaki@mail.huji.ac.il>
2024-09-18 14:25:17 +0300

    cleaner run_model.py

--- run_model.py
+++ run_model.py
@@ -17,6 +17,25 @@ def count_kmers_according_to_lasso(df: pd.DataFrame, kmers: np.array) ->pd.DataF
 
 
 def main():
+    """
+    Main function that loads a pre-trained Lasso model and input data,
+    counts k-mers in the sequences according to model features,
+    generates predictions, and saves the results.
+
+    Steps:
+        1. Load the Lasso model from the specified file.
+        2. Read the input data (CSV format) with sequences indexed by 'id'.
+        3. Count the occurrences of k-mers (as per model features) in the input data sequences.
+        4. Generate predictions using the model based on k-mer counts.
+        5. Save the predictions to a CSV file, with an additional summary in a text file.
+
+    Usage:
+        $ python script.py <model_file> <data_file> <output_name>
+
+        - model_file: Path to the pre-trained Lasso model (joblib format).
+        - data_file: Path to the CSV file containing the sequence data.
+        - output_name: Name used for the output directory and result files.
+    """
     model = joblib.load(sys.argv[1])
     data = pd.read_csv(sys.argv[2], index_col='id')
     name = sys.argv[3]
